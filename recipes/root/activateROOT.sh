aec26bfb79669a674a9f463c6aec57e0acc90c80
Christopher Burr <chrisburr73@googlemail.com>
2016-04-03 10:22:56 +0100

    Set LDFLAGS in activateROOT.sh

--- recipes/root/activateROOT.sh
+++ recipes/root/activateROOT.sh
@@ -1,2 +1,18 @@
 export ROOTSYS=$CONDA_ENV_PATH
-export LD_LIBRARY_PATH=$CONDA_ENV_PATH/lib/root:$LD_LIBRARY_PATH
+export LDFLAGS=-Wl,-rpath,$CONDA_ENV_PATH/lib,-rpath,$CONDA_ENV_PATH/lib/root
+
+if [ ! -z "$LD_LIBRARY_PATH" ]; then
+  echo ""
+  echo "WARNING: LD_LIBRARY_PATH is currently set to '$LD_LIBRARY_PATH'";
+  echo ""
+  echo "Unless this is intentional this probably needs to be unset using: 'unset LD_LIBRARY_PATH'"
+  echo ""
+fi
+
+if [ ! -z "$PYTHONPATH" ]; then
+  echo ""
+  echo "WARNING: PYTHONPATH is currently set to '$PYTHONPATH'";
+  echo ""
+  echo "Unless this is intentional this probably needs to be unset using: 'unset PYTHONPATH'"
+  echo ""
+fi
--- recipes/root/deactivateROOT.sh
+++ recipes/root/deactivateROOT.sh
@@ -1,2 +1,2 @@
 unset ROOTSYS
-export LD_LIBRARY_PATH=$(python -c "'$LD_LIBRARY_PATH'.replace('$CONDA_ENV_PATH/lib/root', '').replace('::', ':')")
+unset LDFLAGS
